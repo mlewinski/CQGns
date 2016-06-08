3dd318b569a5b8f7c9b697b3e3b88b3cb2a5e716
Marek Lewiński <lewinski.marek@outlook.com>
2016-06-08 19:17:30 +0200

    Implemented Hangman game (Challenge 07)

---
+++ Hangman/Hangman/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = {"car", "country", "enigma", "complex", "algorithm", "cat", "dog", "panther", "tiger"};
+            char option = '*';
+            while (option != 'q')
+            {
+                string password = words[(new Random().Next() % words.Length)];
+                string mask = new String('-', password.Length);
+                int wrongAnswers = 0; //failure after 6 wrong answers
+                bool guessed = false;
+                Console.WriteLine("Guess the password!");
+                char letter = '\0';
+                while (!guessed && wrongAnswers < 6)
+                {
+                    Console.WriteLine("Password : {0}", mask);
+                    Console.Write("Letter : ");
+                    letter = Convert.ToChar(Console.ReadLine());
+                    if (password.Contains(letter))
+                    {
+                        Console.WriteLine("Yeah!");
+                        for (int i = password.IndexOf(letter); i < password.Length; i++)
+                        {
+                            if (password[i] == letter)
+                            {
+                                StringBuilder sb = new StringBuilder(mask);
+                                sb[i] = letter;
+                                mask = sb.ToString();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Noooo");
+                        wrongAnswers++;
+                    }
+                    if (!mask.Contains('-')) guessed = true;
+                }
+                Console.WriteLine(guessed ? "Congratulations! You won!" : "You failed!");
+                Console.WriteLine("q - quit, c - continue");
+                try
+                {
+                    option = Convert.ToChar(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    option = 'q';
+                }
+            }
+        }
+    }
+}
