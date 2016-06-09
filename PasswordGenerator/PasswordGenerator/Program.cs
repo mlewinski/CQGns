1499c3bd2b52758f8e45311fd36e0fcc4b8dc25f
Marek Lewiński <lewinski.marek@outlook.com>
2016-06-09 21:03:17 +0200

    Create Password Generator

---
+++ PasswordGenerator/PasswordGenerator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("========== Password Generator ==========");
+            Console.WriteLine("========== Marek Lewinski ==========");
+            Console.WriteLine("Acceptable characters :");
+            string acceptableChars = Console.ReadLine();
+            Console.WriteLine("Number of passwords :");
+            int passwordNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Length :");
+            int length = Convert.ToInt32(Console.ReadLine());
+            StringBuilder charactersBuilder = new StringBuilder(acceptableChars);
+            Random random=new Random();
+            for (int i = 0; i < passwordNumber; i++)
+            {
+                StringBuilder password = new StringBuilder();
+                for (int j = 0; j < length; j++)
+                {
+                    password.Append(charactersBuilder[random.Next()%(acceptableChars.Length)]);
+                }
+                Console.WriteLine(password.ToString());
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
